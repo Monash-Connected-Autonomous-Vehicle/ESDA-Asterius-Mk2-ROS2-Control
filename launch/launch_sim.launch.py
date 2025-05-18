de6e350648ae72906812d4a1c6709d019d895131
neyuh145 <huy.s150103@gmail.com>
2025-05-18 22:27:40 +1000

    fixed robot_core_esda.xacro, the thing still sink in ground plane

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -14,93 +14,93 @@ def generate_launch_description():
     
 
     # Define the input parameters
-    use_sim_time     = LaunchConfiguration('use_sim_time',     default='true')
     pkg = 'esda_vehicle_control'
 
     # ROS Controller Files:
-    robot_controllers = PathJoinSubstitution(
-        [
-            FindPackageShare(pkg),
-            'config',
-            'diff_drive_controller.yaml',
-        ]
-    )
+    # robot_controllers = PathJoinSubstitution(
+    #     [
+    #         FindPackageShare(pkg),
+    #         'config',
+    #         'diff_drive_controller.yaml',
+    #     ]
+    # )
 
     # Launch robot_state_publisher
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory(pkg), 'launch', 'rsp.launch.py')
-        ),
-        launch_arguments={
-            'use_sim_time': 'true',
-            'use_ros2_control': 'true'
-        }.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(pkg),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
+    default_world = os.path.join(
+        get_package_share_directory(pkg),
+        'worlds',
+        'empty.world'
+        )    
+    
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
 
     # --- 2) Launch Ignition sim server + client ---
-    ign_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-          os.path.join(
-            get_package_share_directory('ros_gz_sim'),
-            'launch', 'gz_sim.launch.py'
-          )
-        ),
-        launch_arguments={
-          'ign_args': '-r empty.sdf',  # or your .sdf world
-          'verbose': 'true'
-        }.items()
-    )
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+             )
     
     
     # --- 4) Spawn the robot_description into Ignition ---
-    spawn_entity = Node(
-        package="ros_gz_sim",
-        executable="create",
-        output="screen",
-        arguments=[
-            "-name",  "my_robot_1",
-            "-topic", "robot_description",
-            "-allow_renaming", "true",
-            "--ros-args",
-        ],
-        parameters=[{"use_sim_time": use_sim_time}],
-    )
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'my_bot',
+                                   '-z', '0.1'],
+                        output='screen')
+
 
     # --- 5) Load and start your controllers ---
     # Controller manager spawners
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster"],
-        output='screen'
-    )
-
-    diff_drive_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            'diff_drive_base_controller',
-            '--param-file', robot_controllers,
-            #'--ros-args', '-r', '/diff_drive_base_controller/cmd_vel:=/cmd_vel'
-        ],
-        output='screen'
+        arguments=["joint_broad"],
     )
 
     # --- 6) Set up the Ros Gazebo Bridge ---
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-        output='screen'
+    bridge_params = os.path.join(get_package_share_directory(pkg),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
     )
     
     return LaunchDescription([
-      DeclareLaunchArgument('use_sim_time',    default_value='true'),
-      DeclareLaunchArgument('use_ros2_control',default_value='true'),
-      rsp,
-      bridge,
-      ign_launch,
-      spawn_entity,
-      joint_broad_spawner,
-      diff_drive_spawner,
+        rsp,
+        world_arg,
+        gazebo,
+        spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])
