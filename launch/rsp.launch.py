fff7eddd9d57795c571dba057f5b95c3f2185eeb
neyuh145 <huy.s150103@gmail.com>
2025-05-31 17:46:01 +1000

    buncha shit copied from tutorials

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -6,7 +6,6 @@ from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
 
 import xacro
 
@@ -23,10 +22,8 @@ def generate_launch_description():
     # robot_description_config = xacro.process_file(xacro_file).toxml()
     robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
-    # Wrap the robot_description in ParameterValue
-    robot_description = ParameterValue(robot_description_config, value_type=str)
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description, 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -34,24 +31,6 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    # Static transform: map → odom → base_link
-    
-    static_tf_node_1 = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_tf_map_to_odom',
-        arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
-        output='screen'
-    )
-
-    static_tf_node_2 = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_tf_odom_to_base_link',
-        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link'],
-        output='screen'
-    )
-    
 
     # Launch!
     return LaunchDescription([
@@ -63,8 +42,6 @@ def generate_launch_description():
             'use_ros2_control',
             default_value='true',
             description='Use ros2_control if true'),
-        static_tf_node_1,
-        static_tf_node_2,
+
         node_robot_state_publisher
-    ],
-    )
+    ])
